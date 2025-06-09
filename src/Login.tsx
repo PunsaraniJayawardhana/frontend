6744f739efa4b14a1ffd45d4ab510f9b759849ac
PunsaraniJayawardhana <punsaranijayawardhana@gmail.com>
2025-06-09 12:55:16 +0530

    cicd

--- src/Login.tsx
+++ src/Login.tsx
@@ -10,7 +10,7 @@ const Login: React.FC = () => {
   const handleLogin = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
-      const res = await axios.post('https://backend-app-with-testing.netlify.app/api/auth/login', {
+      const res = await axios.post('http://localhost:5000/api/auth/login', {
         email,
         password,
       });
